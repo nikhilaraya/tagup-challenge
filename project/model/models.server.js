e144fb21c4a237387ddd43ee7f089bb358ef2e89
Nikhila Raya <nikhila.raya@gmail.com>
2019-04-01 23:03:49 -0400

    mongo connection url  commit

--- project/model/models.server.js
+++ project/model/models.server.js
@@ -1,11 +1,5 @@
 var mongoose = require("mongoose");
 var connectionString = 'mongodb://localhost/nextJump';
-if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-  var password = process.env.MLAB_PASSWORD_WEBDEV;
-  connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds129394.mlab.com:29394/heroku_d7gpxsv2'; // use yours
-}
 
 var db = mongoose.connect(connectionString, {useMongoClient: true});
 module.exports = db;
